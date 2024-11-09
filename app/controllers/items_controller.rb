29b27e0ccb90d06fc55d92cc23cc3b08a15cff76
Brian Song <brian.cs.song@gmail.com>
2024-11-09 16:42:05 +0000

    Changes for the link-in-bio-form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
+  
+  def save_item
+    x = Item.new
+    x.link_url = params.fetch("link_url_param")
+    x.link_description = params.fetch("link_description_param")
+    x.thumbnail_url = params.fetch("thumbnail_url_param")
+    x.save
+
+    # render ({ :template => "item_templates/process_form"})
+    
+    redirect_to("/")
+  end
+
+  def display_form
+    render({ :template => "item_templates/new_item_form" })
+  end
+
   def index
     @list_of_items = Item.all
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", { :controller => "items", :action => "display_form" })
+
+  post("/add_item_row", { :controller => "items", :action => "save_item" })
+
 end
